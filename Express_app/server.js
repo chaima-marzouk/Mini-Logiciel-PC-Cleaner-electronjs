202d95b464a3c1e05af691ef574a310676fbe858
chaima marzouk <marzouk.chaima.892@gmail.com>
2022-06-06 14:04:23 +0100

    💬 create server

---
+++ Express_app/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv  from 'dotenv';
+const app =  express();
+// import AuthRoute  from './src/router/Auth.router';
+// import AnalyseRoute  from './src/router/analyse.route';
+import './src/db/database';
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// app.use('/api/users', AuthRoute);
+// app.use('/', AnalyseRoute);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
