88aa3740c111f7af2a9c78d23ea93f912bae7263
chaima marzouk <marzouk.chaima.892@gmail.com>
2022-06-06 14:04:48 +0100

    🔇 🙈 connect to db

---
+++ Express_app/src/db/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const DB = process.env.DATABASE_LOCAL;
+exports = mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+   
+    console.log('Connection successed !! ');
+
+}).catch(error => {
+
+    console.log(error);
+
+})
